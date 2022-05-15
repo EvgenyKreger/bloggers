d0bfdc5b73dfc2effcf8608241466bd70509a43f
Evgeny Kreger <84592469+EvgenyKreger@users.noreply.github.com>
2022-05-15 22:40:56 +0300

    Update db.ts

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,7 +1,7 @@
 import {MongoClient} from "mongodb";
 
 
-const mongoUri = process.env.mongoURI || 'mongodb+srv://KEA:%23%2E8M%24T5crMKABnQ@kea-cluster.ayru5.mongodb.net/MyFirstProject?retryWrites=true&w=majority'
+const mongoUri = process.env.mongoURI || "http://localhost:5001"
 
 export const client = new MongoClient(mongoUri)
 
